279ee2a441d1a449b859255b1e73f950bb86f1d0
victormichelsilva <victormichelsilva@gmail.com>
2021-12-06 19:21:00 -0300

    Adicionar arquivos de projeto.

---
+++ Email.cs
@@ -0,0 +1,18 @@
+﻿namespace BuilderPadrao
+{
+    public class Email
+    {
+        public string To { get; set; }
+
+        public string From { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return ($"Destino: {To} - Origem: {From} \nAssunto: {Subject}\n \t{Body}\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BuilderPadrao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailBuilder = new EmailBuilder()
+                                .To("microsoft@email.com")
+                                .From("victormichelsilva@gmail.com")
+                                .Subject("Microsoft Build")
+                                .Body("Microsoft Build 2021\n\t25 a 27 de Maio 2021" +
+                                        "\n\tNovidades Windows, Azure, Office e mais.")
+                                .CriaEmail();
+
+            Console.WriteLine(emailBuilder.ToString());
+            Console.ReadLine();
+        }
+    }
+}
